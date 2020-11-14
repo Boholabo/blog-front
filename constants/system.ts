20bd664df6296a844ea7b2661d48ea821c58b3e4
mikinovation <maneuver2472@gmail.com>
2020-11-14 23:24:58 +0800

    systemの追加

---
+++ constants/system.ts
@@ -0,0 +1,38 @@
+export const Language = {
+  Ja: 'ja',
+  En: 'en',
+};
+
+export const Theme = {
+  Default: 'default',
+  Dark: 'dark',
+};
+
+export const Route = {
+  Index: 'index',
+  ArticleDetail: 'article-article_id',
+  SearchArchive: 'search-keyword',
+  Service: 'service',
+  App: 'app',
+  About: 'about',
+  Sitemap: 'sitemap',
+};
+
+export const ImageExt = {
+  WebP: 'webp',
+  Jpg: 'jpeg',
+};
+
+export const StorageField = {
+  Theme: 'theme',
+  User: 'user',
+  UserLikeHistory: 'user_like_history',
+};
+
+export default {
+  Language,
+  Theme,
+  Route,
+  ImageExt,
+  StorageField,
+};
