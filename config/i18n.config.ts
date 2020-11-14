2c4798ebbc14705d6ba18371ac660845368ca38d
mikinovation <maneuver2472@gmail.com>
2020-11-14 23:18:00 +0800

    i18n.config追加

---
+++ config/i18n.config.ts
@@ -0,0 +1,313 @@
+export default {
+  default: 'ja',
+  languages: [
+    {
+      code: 'ja',
+      iso: 'ja-JP',
+      name: '日本語',
+    },
+    {
+      code: 'en',
+      iso: 'en-US',
+      name: 'English',
+    },
+  ],
+  data: {
+    nav: {
+      home: {
+        ja: '',
+        en: 'home',
+      },
+      think: {
+        ja: '',
+        en: 'think',
+      },
+      code: {
+        ja: '',
+        en: 'code',
+      },
+      project: {
+        ja: '',
+        en: 'project',
+      },
+      about: {
+        ja: '',
+        en: 'about',
+      },
+      service: {
+        ja: '',
+        en: 'service',
+      },
+      app: {
+        ja: '',
+        en: 'app',
+      },
+      map: {
+        ja: '',
+        en: 'sitemap.xml',
+      },
+    },
+    text: {
+      empty: {
+        ja: '',
+        en: 'No Result Data.',
+      },
+      friendMe: {
+        ja: '',
+        en: 'Friend me',
+      },
+      notFound: {
+        ja: '',
+        en: 'Arrives here at last',
+      },
+      backToHomePage: {
+        ja: '',
+        en: 'Driveby',
+      },
+      service: {
+        slogan: {
+          ja: '',
+          en: 'Talk is cheap. Show me the code',
+        },
+        emailMe: {
+          ja: '',
+          en: 'Email ME',
+        },
+        hireMe: {
+          ja: '',
+          en: 'Hire me on upwork.com',
+        },
+      },
+      device: {
+        android: {
+          ja: '',
+          en: 'Android',
+        },
+        ios: {
+          ja: '',
+          en: 'IOS',
+        },
+      },
+      feedback: {
+        ja: '',
+        en: 'Feedback',
+      },
+      totop: {
+        ja: '',
+        en: 'To top',
+      },
+      tobottom: {
+        ja: '',
+        en: 'Next screen',
+      },
+      action: {
+        open: {
+          ja: '',
+          en: 'open description',
+        },
+        close: {
+          ja: '',
+          en: 'close description',
+        },
+        on: {
+          ja: '',
+          en: 'on',
+        },
+        off: {
+          ja: '',
+          en: 'off',
+        },
+      },
+      slogan: {
+        ja: '',
+        en: 'Because the mountain was there.',
+      },
+      category: {
+        name: {
+          ja: '',
+          en: 'Categories',
+        },
+        empty: {
+          ja: '',
+          en: 'no cate',
+        },
+      },
+      search: {
+        ja: '',
+        en: 'Search...',
+      },
+      tag: {
+        name: {
+          ja: '',
+          en: 'Tags',
+        },
+        empty: {
+          ja: '',
+          en: 'No tags.',
+        },
+      },
+      article: {
+        fullcolread: {
+          ja: '',
+          en: 'Full Column',
+        },
+        fullscreenread: {
+          ja: '',
+          en: 'Full Screen',
+        },
+        readAll: {
+          ja: '',
+          en: 'Read all',
+        },
+        rendering: {
+          ja: '',
+          en: 'rendering...',
+        },
+        name: {
+          ja: '',
+          en: 'Articles',
+        },
+        empty: {
+          ja: '',
+          en: 'No Result Article.',
+        },
+        hotlist: {
+          ja: '',
+          en: 'hot',
+        },
+        loadmore: {
+          ja: '',
+          en: 'loadmore',
+        },
+        loading: {
+          ja: '',
+          en: 'loading',
+        },
+        nomore: {
+          ja: '',
+          en: 'no more',
+        },
+      },
+      page: {
+        name: {
+          ja: '',
+          en: 'pages',
+        },
+      },
+      announcement: {
+        empty: {
+          ja: '',
+          en: 'No announcements.',
+        },
+      },
+      comment: {
+        empty: {
+          ja: '',
+          en: 'Hit the nail on the head.',
+        },
+        anonymous: {
+          ja: '',
+          en: 'anonymous',
+        },
+        ding: {
+          ja: '',
+          en: 'like',
+        },
+        reply: {
+          ja: '',
+          en: 'reply',
+        },
+        pagenation: {
+          new: {
+            ja: '',
+            en: 'new',
+          },
+          old: {
+            ja: '',
+            en: 'old',
+          },
+        },
+        profile: {
+          name: {
+            ja: '',
+            en: 'name',
+          },
+          email: {
+            ja: '',
+            en: 'email',
+          },
+          site: {
+            ja: '',
+            en: 'site',
+          },
+          content: {
+            ja: '',
+            en: 'content',
+          },
+          emailerr: {
+            ja: '',
+            en: 'Email address is not legitimate!',
+          },
+          siteerr: {
+            ja: '',
+            en: 'Site is not legitimate!',
+          },
+          contenterr: {
+            ja: '',
+            en: 'Content requirements are within 2000 words / 36 lines!',
+          },
+          submiterr: {
+            ja: '',
+            en: 'Submit err, get err in dev console!',
+          },
+          actionerr: {
+            ja: '',
+            en: 'Action err, get err in dev console!',
+          },
+        },
+        setting: {
+          account: {
+            ja: '',
+            en: 'account setting',
+          },
+          edit: {
+            ja: '',
+            en: 'edit profile',
+          },
+          clear: {
+            ja: '',
+            en: 'clear profile',
+          },
+        },
+        placeholder: {
+          ja: '',
+          en: 'Hit the nail on the head.',
+        },
+        count: {
+          ja: '',
+          en: 'comments',
+        },
+        like: {
+          ja: '',
+          en: ' likes',
+        },
+        new: {
+          ja: '',
+          en: 'new',
+        },
+        hot: {
+          ja: '',
+          en: 'hot',
+        },
+        submit: {
+          ja: '',
+          en: 'publish',
+        },
+        submiting: {
+          ja: '',
+          en: 'publishing',
+        },
+      },
+    },
+  },
+};
