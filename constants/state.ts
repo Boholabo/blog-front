a14ee8bbb188d8ba5b16955f2f60713d1f743c32
mikinovation <maneuver2472@gmail.com>
2020-11-14 23:22:08 +0800

    stateの追加

---
+++ constants/state.ts
@@ -0,0 +1,26 @@
+export const OriginState = {
+  Original: 0,
+  Reprint: 1,
+  Hybrid: 2,
+};
+
+export const CommentPostType = {
+  Guestbook: 0,
+};
+
+export const CommentParentType = {
+  Self: 0,
+};
+
+export const SortType = {
+  Asc: 1,
+  Desc: -1,
+  Hot: 2,
+};
+
+export default {
+  OriginState,
+  CommentPostType,
+  CommentParentType,
+  SortType,
+};
